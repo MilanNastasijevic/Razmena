0da3d8b526fdde2ca4edd05e910f00265dd5ab68
MilanNastasijevic <milan.nastasijevic@gmail.com>
2017-03-24 01:36:11 +0100

    Conflicts resolved from merging with pokusaj

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
 import git_testiranje from milan_git
 
 
-<<<<<<< HEAD
 hocu konflikt
 hocu konflikt
 
@@ -18,16 +17,15 @@ hocu konflikt
 hocu konflikt
 hocu konflikt
 hocu konflikt
+
 testiranje testiranje!!!
-=======
-dule dule
-dule dule
-dule dule
-dule dule
-dule dule
-dule dule
 
+dule dule
+dule dule
+dule dule
+dule dule
+dule dule
+dule dule
 milan milan
->>>>>>> pokusaj
 
 print test
